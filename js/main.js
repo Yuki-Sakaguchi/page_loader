72df0dd1bdf1b74711a56a9ba3845710942c66f6
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2017-07-01 10:53:22 +0900

    ローディングの時間を調整

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 $(function() {
-  $(window).on('load', function() {
+  var
+  $window = $(window),
+  $ball   = $('.ball'),
+  $shadow = $('.shadow'),
+  $loader = $('.loader');
+
+  var
+  bounsClass = 'js-bouns',
+  shadowClass = 'js-shadow',
+  scaleUpClass = 'js-scale-up';
+
+  $window.on('load', function() {
     setTimeout(function() {
-      $('.loaded-content').fadeOut(1000);
-    }, 1000);
+      $loader.fadeOut(1000, function() {
+        $loader.remove();
+      });
+    }, 2000);
   });
 });
